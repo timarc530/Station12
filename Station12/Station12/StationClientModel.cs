1816a7a10e7f85c26924062849c121a4e61ae414
cdhanna <cdhanna@wpi.edu>
2014-08-09 19:13:11 -0400

    basic models

---
+++ Station12/Station12/StationClientModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SmallNet;
+namespace Station12
+{
+    class StationClientModel : DefaultClientModel
+    {
+        public override void destroy()
+        {
+            //throw new NotImplementedException();
+        }
+
+        protected override void gotMessage(SMessage message)
+        {
+           // throw new NotImplementedException();
+        }
+
+        public override void init()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void playerJoined(int id)
+        {
+           // throw new NotImplementedException();
+        }
+
+        public override void update(Microsoft.Xna.Framework.GameTime time)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override bool validateMessage(SMessage message)
+        {
+           // throw new NotImplementedException();
+            return true;
+        }
+    }
+}
---
+++ Station12/Station12/StationHostModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Station12
+{
+    class StationHostModel
+    {
+    }
+}
