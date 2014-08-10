7f026cddbe2fd5fdee8cc987f9c1dd6e1781739e
cdhanna <cdhanna@wpi.edu>
2014-08-10 18:51:31 -0400

    orbitz

--- Station12/Station12/Game1.cs
+++ Station12/Station12/Game1.cs
@@ -114,8 +114,29 @@ namespace Station12
             planet.Sprite.autoCenter();
             planet.Sprite.Scale *= .8f;
             int radius = Math.Min(planet.Sprite.Image.Height,planet.Sprite.Image.Width)/2;
+           // planet.setMotionFunction((spr, time) => { });
             menuBackground.addObject(planet);
 
+
+            //make stars
+            SimpleMotionObject[] stars = new SimpleMotionObject[8];
+            Texture2D starImg = Content.Load<Texture2D>("stars1.png");
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new SimpleMotionObject(starImg);
+                stars[i].Sprite.Position = new Vector2(screenX * (float)random.NextDouble(), screenY * (float)random.NextDouble());
+                //stars[i].Sprite.Color = ColorPalette.NEBULA;
+                stars[i].Sprite.autoCenter();
+                stars[i].Sprite.Depth = .4f;
+                stars[i].Sprite.Scale *= .5f;
+                float dir = Math.Sign((float)random.NextDouble() - .5);
+                float speed = dir * (float)random.NextDouble() * .00001f;
+                stars[i].setMotionFunction((spr, time) =>
+                {
+                    spr.Rotation += speed;
+                });
+                menuBackground.addObject(stars[i]);
+            }
 
             //make clouds
             SimpleMotionObject[] nebulas = new SimpleMotionObject[4];
@@ -127,7 +148,7 @@ namespace Station12
                 nebulas[i].Sprite.Color = ColorPalette.NEBULA;
                 nebulas[i].Sprite.autoCenter();
                 nebulas[i].Sprite.Depth = .5f;
-                nebulas[i].Sprite.Scale *= 2;
+                nebulas[i].Sprite.Scale *= 1.7f;
                 float dir = Math.Sign((float)random.NextDouble()-.5);
                 float speed = dir*(float)random.NextDouble() * .003f;
                 nebulas[i].setMotionFunction((spr, time) => {
@@ -143,7 +164,7 @@ namespace Station12
             tobj.Sprite.Scale *= .5f;
             tobj.Sprite.autoCenter();
             float angle = 0;
-            double ecA = screenX * .45f;
+            double ecA = screenX * .35f;
             double ecB = 80;
             double ecPhi = 0;
             int flip = 0;
@@ -153,25 +174,35 @@ namespace Station12
                 Vector2 oldpos = spr.Position;
                 bool transition;
                 double f = (ecA * ecB) / Math.Sqrt(Math.Pow(ecB * Math.Cos(angle), 2) + Math.Pow(ecA * Math.Sin(angle), 2));
-                angle += .01f;
+                float modifer = (float)(ecA - ecB) / (planetOrigin - spr.Position).Length();
+                angle +=.001f* modifer;
+                angle = angle % (float)(Math.PI * 2);
                 Vector2 traj = new Vector2((float)Math.Cos(angle + ecPhi), (float)Math.Sin(angle + ecPhi));
                 Vector2 add = ((float)f) * traj;
                 spr.Position = planetOrigin + add;
                 float v1 = (oldpos - planetOrigin).Length();
                 float v2 = (spr.Position - planetOrigin).Length();
-                transition = ((v1 > 200) ^ (v2 > 200));
+                transition = ((v1 > radius) ^ (v2 > radius));
+                
                 if (transition)
-                {
+                {    
                     flip++;
                     flip = flip % 4;
                 }
 
                 if (flip ==3)
                 {
+                    
                     spr.Depth = 0.4f;
                 }
                 else
                     spr.Depth = 0.7f;
+
+
+                
+
+
+            
             });
 
         }
@@ -204,6 +235,7 @@ namespace Station12
 
             menuBackground.update(gameTime);
 
+            camera.Update(gameTime);
             mouse.Update();
             base.Update(gameTime);
         }
